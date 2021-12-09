a0709c43c654f4bb442a1ea465d0099b8d81df35
Lucas Lopes <Lucasrlopes2002@gmail.com>
2021-12-09 11:50:54 -0300

    nested attributes with has_many, aula: 23 mod: 1

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -4,9 +4,9 @@ class Contact < ApplicationRecord
   belongs_to :kind #, optional:true
   has_many :phones
 
-  def as_json(options = {})
+  def as_json(options={})
     h = super(options)
-    h[:birthdate] = (i18n.l(self.birthdate)) unless slef.birthdate.blank?)
+    h[:birthdate] = (I18n.l(self.birthdate) unless self.birthdate.blank?)
     h
   end
 
