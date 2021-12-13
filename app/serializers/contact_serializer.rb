289365223b0850bb66a74e988e4b47d0bad469e6
Lucas Lopes <Lucasrlopes2002@gmail.com>
2021-12-13 10:53:11 -0300

    Associations, aula: 31 mod: 2

--- app/serializers/contact_serializer.rb
+++ app/serializers/contact_serializer.rb
@@ -1,6 +1,12 @@
 class ContactSerializer < ActiveModel::Serializer
   attributes :id, :name, :email, :birthdate
 
+  # assosiations
+  belongs_to :kind
+  has_many :phones
+  has_one :address
+
+
   def as_json(*args)
     h = super(*args)
     # pt_BR ------> h[:birthdate] = (I18n.l(object.birthdate) unless object.birthdate.blank?)
